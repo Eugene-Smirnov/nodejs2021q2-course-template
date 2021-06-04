19aee5b7c99e51becb8d27d13246345af3974ea0
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-06-04 14:04:32 +0300

    feat: changed .eslint and package.json files to treat errors moduleOptions.option

--- src/resources/boards/board.model.ts
+++ src/resources/boards/board.model.ts
@@ -2,14 +2,12 @@ import { v4 } from 'uuid';
 
 class Board {
   public id: string;
+
   public title: string;
+
   public columns: Set<string>;
 
-  constructor({
-    id = v4.uuid(),
-    title = 'BOARD',
-    columns = new Set(['']),
-  } = {}) {
+  constructor({ id = v4(), title = 'BOARD', columns = new Set(['']) } = {}) {
     this.id = id;
     this.title = title;
     this.columns = columns;
