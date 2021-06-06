8b8ccc5b5eee0f58a0c490f25b12e405344c1f6c
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-06-06 20:35:25 +0300

    feat: implement uncaughtException and unhandledRejection logging

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
 import { config } from './common/config';
 
 import { app } from './app';
-// import { log } from './logging/log';
-// import { errJournalLog } from './logging/log-errjournal';
+import { log } from './logging/log';
+import { errJournalLog } from './logging/log-errjournal';
 
 app.listen(config.PORT, () =>
   console.log(`App is running on http://localhost:${config.PORT}`)
 );
+
+process.on('uncaughtException', (err: Error, origin: string) => {
+  const content = `----
+  UNCAUGHT EXCEPTION
+  ----
+  Caught exception: ${JSON.stringify(err)}
+  Exception origin: ${origin}`;
+  log(content);
+  errJournalLog(content);
+});
+
+process.on('unhandledRejection', (reason: Error, promise) => {
+  const content = `----
+  UNHANDLED REJECTION
+  ----
+  Unhandled Rejection at: ${JSON.stringify(promise)}
+  Reason: ${JSON.stringify(reason)}`;
+  log(content);
+  errJournalLog(content);
+});
