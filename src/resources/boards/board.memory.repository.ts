21f3a6faaab40b3945eae10cc586ef10068ca4fa
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-06-27 01:46:53 +0300

    feat: move board repo to pgsql using typeorm

--- src/resources/boards/board.memory.repository.ts
+++ src/resources/boards/board.memory.repository.ts
@@ -1,16 +1,14 @@
 import { Board } from './board.model';
 import { removeByBoardId as removeBoardTasks } from '../tasks/task.memory.repository';
 
-const storedBoards: Board[] = [];
-
-export const getAll = async (): Promise<Board[]> => storedBoards as Board[];
+export const getAll = async (): Promise<Board[]> => Board.find();
 
 export const getById = async (id: string): Promise<Board | undefined> =>
-  Promise.resolve(storedBoards.find((board: Board) => board.id === id));
+  Board.findOne(id);
 
 export const create = async (boardDto: Board): Promise<Board> => {
   const board = new Board(boardDto);
-  storedBoards.push(board);
+  await board.save();
   return board;
 };
 
@@ -20,12 +18,13 @@ export const update = async (
 ): Promise<Board> => {
   const board = await getById(id);
   const updatedBoard = new Board({ ...board, ...boardUpdateDto });
-  Object.assign(board, boardUpdateDto);
+  await Board.update(id, updatedBoard);
   return updatedBoard;
 };
 
 export const remove = async (id: string): Promise<void> => {
-  const index = storedBoards.findIndex((board) => board.id === id);
+  const removedBoard = await getById(id);
+  if (!removedBoard) throw new Error('Board not found!');
   await removeBoardTasks(id);
-  storedBoards.splice(index, 1);
+  Board.remove(removedBoard);
 };
